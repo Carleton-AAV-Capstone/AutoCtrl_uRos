61da6cc516f0d38e1681904c865901745ef578f8
saudm3012 <immsaud@gmail.com>
2025-01-28 15:43:52 -0500

    Enabled RC control

--- src/esp32_ros/AAV_fns/AAV_fns.cpp
+++ src/esp32_ros/AAV_fns/AAV_fns.cpp
@@ -14,17 +14,17 @@ void throttle_callback_ackermann(const void * msgin){
   const ackermann_msgs__msg__AckermannDrive * msg = (const ackermann_msgs__msg__AckermannDrive *)msgin;
 
   //USER_SERIAL.println("recv");
-  //USER_SERIAL.print(readSwitch(USE_RC, false));
+  USER_SERIAL.print(readSwitch(USE_RC, false));
   digitalWrite(2, HIGH);
-  //if (msg != NULL || readSwitch(USE_RC, false)) {
-  if (msg != NULL) {
+  if (msg != NULL || readSwitch(USE_RC, false)) {
+  // if (msg != NULL) {
     
     uint16_t accelMag;
     bool dir;
    
     
     
-    //if(!readSwitch(USE_RC, false)){
+    if(!readSwitch(USE_RC, false)){
 
       if(msg->acceleration > 0){
         digitalWrite(DIR_PIN, LOW);
@@ -41,11 +41,18 @@ void throttle_callback_ackermann(const void * msgin){
       USER_SERIAL.println(msg->steering_angle);
       
       steer_vals.setpoint = (int) map((long) msg->steering_angle,-2000, 2000, 100, 0);
-    // }else{
-    //   setpoint = map((long) readChannel(STR_RC, -255, 255, 0),-3200, 3200, 0, 100);
-    //   accelMag = readChannel(THR_RC, -2000, 2000, 0);
-    //   //USER_SERIAL.println("USING RC");
-    // }
+    }else{
+      steer_vals.setpoint = map((long) readChannel(STR_RC, -255, 255, 0),-3200, 3200, 0, 100);
+      accelMag = readChannel(THR_RC, -2000, 2000, 0);
+      if(!readSwitch(REV_EN,false)){
+        digitalWrite(DIR_PIN, LOW);
+        digitalWrite(LED_PIN, LOW);
+      } else {
+        digitalWrite(DIR_PIN, HIGH);
+        digitalWrite(LED_PIN, HIGH);
+      }
+      USER_SERIAL.println("USING RC");
+    }
     if(!dir){
       USER_SERIAL.print("Steering Angle: "); USER_SERIAL.print((int) setpoint);
       USER_SERIAL.print("   voltage level(drv): ");
